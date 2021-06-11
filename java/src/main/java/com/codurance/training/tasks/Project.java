039b2552044068784a088650ddf0325d346df8bc
maksym.dotsenko <maksym.dotsenko@ebcont.com>
2021-06-11 14:07:47 +0200

    add Project

---
+++ java/src/main/java/com/codurance/training/tasks/Project.java
@@ -0,0 +1,17 @@
+package com.codurance.training.tasks;
+
+import java.util.List;
+
+public class Project {
+    private String name;
+    private  List tasks;
+
+    public String getName() {
+        return name;
+    }
+
+    public List getTasks() {
+        return tasks;
+    }
+
+}
